ffdfe19e2a8c04c45b59ae0f880255828490155a
Marc Andrei <santosmarcandrei1414@gmail.com>
2024-11-15 16:31:07 +0800

    Add project files.

---
+++ MyPortfolio/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// Function to open the modal and display the image
+function openModal(imageSrc) {
+    const modal = document.getElementById("imageModal");
+    const modalImage = document.getElementById("modalImage");
+
+    // Set the source of the image in the modal to the clicked image
+    modal.style.display = "flex";  // Show the modal
+    modalImage.src = `/images/${imageSrc}`;  // Set the src to the clicked image
+}
+
+// Function to close the modal
+function closeModal() {
+    const modal = document.getElementById("imageModal");
+    modal.style.display = "none";  // Hide the modal
+}
+
+
+
